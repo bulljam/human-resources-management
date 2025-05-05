14af998c1e06b739161796a6df4d305c6204e776
Aymane Bouljam <235529830+bulljam@users.noreply.github.com>
2025-05-05 14:20:10 +0100

    feat: implement admin livewire components and views

---
+++ app/Livewire/Admin/Designation/Create.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin\Designation;
+
+use Livewire\Component;
+
+class Create extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.designation.create');
+    }
+}
